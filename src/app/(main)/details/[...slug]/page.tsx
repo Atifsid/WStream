35c4679ef76b19818f0126fc2feb4f3aefad2f90
Atifsid <atifsiddiqui35@gmail.com>
2024-04-27 01:29:03 +0530

    handle nothing found error

--- src/app/(main)/details/[...slug]/page.tsx
+++ src/app/(main)/details/[...slug]/page.tsx
@@ -1,3 +1,7 @@
 export default function Details() {
-  return <div>Work in progress</div>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="text-3xl">Work in progress!</h1>
+    </div>
+  );
 }
--- src/app/ui/select/SearchMain.tsx
+++ src/app/ui/select/SearchMain.tsx
@@ -2,6 +2,10 @@ import Card from "@/app/components/Card";
 import Link from "next/link";
 
 export default function SearchMain({ data }: { data: any }) {
+  if (data && data.Error) {
+    return <div className="mt-4">{data.Error}</div>;
+  }
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 max-w-md md:max-w-6xl mx-auto py-2">
       {data &&
